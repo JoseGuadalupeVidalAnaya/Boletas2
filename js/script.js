52882c561fb8eff2414c127eb97217e399cda66f
Jose Guadalupe Vidal Anaya <joseguadalupevidalanaya@gmail.com>
2018-05-07 17:05:10 -0500

    inicio de proyecto

---
+++ js/script.js
@@ -0,0 +1,8 @@
+M.AutoInit();
+$(".dropdown-button").dropdown();
+$(".dropdown-button2").dropdown();
+$(document).ready(function ()
+{
+    $('.sidenav').sidenav();
+    $('input#controlA, input#control').characterCounter();
+});
